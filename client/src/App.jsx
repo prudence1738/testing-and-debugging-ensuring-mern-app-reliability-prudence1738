2d741bb71e077e2839bcafd0d4273a2438af12ba
Prudence Oyando <prudenceoyando98@gmail.com>
2025-11-27 11:36:44 +0300

    mern-testing/client/src/App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './components/Login';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Example MERN Testing App</h1>
+      <Login onLogin={() => {}} />
+    </div>
+  );
+}
